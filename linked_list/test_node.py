ae64829dec3ef39711650181012a5e641d3ea406
Cedar Williams <33876916+cedar-williams@users.noreply.github.com>
2026-06-26 17:46:01 -0400

    Create test_node.py Create simple pytest to validate node class

---
+++ linked_list/test_node.py
@@ -0,0 +1,8 @@
+from Node import Node
+
+def test_new_node_is_empty():
+    test_node = Node("test string")
+
+    assert test_node.data == "test string"
+    assert test_node.next_node is None
+    assert test_node.prev_node is None
