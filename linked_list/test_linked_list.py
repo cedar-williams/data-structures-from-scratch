a07136f4cb8182699785b5e1ef53fc00699d9100
Cedar Williams <33876916+cedar-williams@users.noreply.github.com>
2026-06-26 18:08:01 -0400

    Create test_linked_list.py

---
+++ linked_list/test_linked_list.py
@@ -0,0 +1,32 @@
+from linked_list import LinkedList
+from node import Node
+
+
+def test_new_list_is_empty():
+    test_list = LinkedList()
+
+    assert test_list.head is None
+    assert test_list.tail is None
+
+def test_new_list_with_one_node():
+    test_data = "test data"
+    test_list = LinkedList()
+    test_list.add(test_data)
+
+    assert test_list.head is test_list.tail
+    assert test_list.head.data == test_data
+
+def test_add_new_node_to_existing_list():
+    first_test_data = "first test data"
+    second_test_data = "second test data"
+    test_list = LinkedList()
+    test_list.add(first_test_data)
+    test_list.add(second_test_data)
+
+    assert test_list.head is not test_list.tail
+    assert test_list.head.data == first_test_data
+    assert test_list.tail.data == second_test_data
+    assert test_list.head.next_node is test_list.tail
+    assert test_list.tail.prev_node is test_list.head
+    assert test_list.head.prev_node is None
+    assert test_list.tail.next_node is None
