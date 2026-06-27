9178ba2fca9018c331e4f11edd515e628e5ae4a7
Cedar Williams <33876916+cedar-williams@users.noreply.github.com>
2026-06-26 20:19:40 -0400

    Add pop() and __len__()

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -1,11 +1,14 @@
+from typing import Any
+
 from node import Node
 
 class LinkedList:
     """Linked list implementation"""
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.head = None
         self.tail = None
+        self.size = 0
 
     def push(self, data) -> None:
         """Add a new node at end of list
@@ -18,3 +21,27 @@ class LinkedList:
             self.tail.next_node = node
             node.prev_node = self.tail
             self.tail = node
+
+        self.size += 1
+
+    def pop(self) -> Any:
+        """Remove the last node from the list
+        :return: Data of node, None if list is empty"""
+        if self.head is None:
+            raise IndexError("List is empty")
+        else:
+            self.size -= 1
+            return_data = self.tail.data
+            if self.head is self.tail:
+                self.head = None
+                self.tail = None
+            else:
+                self.tail.prev_node.next_node = None
+                self.tail = self.tail.prev_node
+
+            return return_data
+
+    def __len__(self) -> int:
+        """:return: number of nodes in list"""
+        return self.size
+
