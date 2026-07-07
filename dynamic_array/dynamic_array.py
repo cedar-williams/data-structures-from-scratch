c7d2b7f9eee35f4be1825f6f61fe17108ac4e4d9
Cedar Williams <33876916+cedar-williams@users.noreply.github.com>
2026-07-07 16:32:12 -0400

    Starting on DynamicArray

---
+++ dynamic_array/dynamic_array.py
@@ -0,0 +1,69 @@
+from typing import Any, Generic, TypeVar, List
+from dynamic_array_iterator import DynamicArrayIterator
+
+class DynamicArray:
+    """Dynamic array of generic type"""
+
+    def __init__(self, capacity = 10) -> None:
+        self.length = 0
+        self.capacity = capacity
+        self.list = []
+
+    def append(self, item):
+        """Add item to the array"""
+        self.list.append(item)
+        self.length += 1
+        self._update_capacity()
+
+    def get(self, index: int) -> Any:
+        """Returns item at specified index
+        :raises IndexError: if index is invalid"""
+        if index < 0 or index >= self.length:
+            raise IndexError("Requested index is outside of array")
+        return self.list[index]
+
+    #Capacity changes
+    def _update_capacity(self):
+        """Resize if needed"""
+        if self.length == self.capacity:
+            self._grow()
+        elif (self.length <= self.capacity / 2) and (self.capacity > 10):
+            self._shrink()
+
+    def _grow(self):
+        """Increase the capacity of the array"""
+        self.capacity *= 2
+
+    def _shrink(self):
+        self.capacity /= 2
+
+    def __eq__(self, other: Any) -> bool:
+        """O(n)
+        return true if both DynamicArray's match, false if not"""
+        if not isinstance(other, DynamicArray):
+            return NotImplemented
+
+        if len(self) != len(other):
+            return False
+        for i in range(len(self)):
+            if self.get(i) != other.get(i):
+                return False
+        return True
+
+    def __len__(self) -> int:
+        return self.length
+
+    def __iter__(self):
+        return DynamicArrayIterator(self)
+
+    def __contains__(self, item):
+        for i in self:
+            if i == item:
+                return True
+        return False
+
+    def __str__(self):
+        string = "DynamicArray[ "
+        for item in self:
+            string += str(item) + " "
+        return string + "]"
