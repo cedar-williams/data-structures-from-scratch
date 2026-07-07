3f5474501868a92216b3fc3ff845b7f8df9fa728
Cedar Williams <33876916+cedar-williams@users.noreply.github.com>
2026-07-07 16:40:51 -0400

    Add clear docstring comments

--- dynamic_array/dynamic_array_iterator.py
+++ dynamic_array/dynamic_array_iterator.py
@@ -1,12 +1,20 @@
+from typing import Any, Self
+
 class DynamicArrayIterator:
+    """An iterator that traverses the array from 0 through end"""
+
     def __init__(self, dynamic_array) -> None:
+        """Initialize the iterator at the start of the array"""
         self.dynamic_array = dynamic_array
         self.cur_index = 0
 
-    def __iter__(self):
+    def __iter__(self) -> Self:
+        """Return this iterator"""
         return self
 
-    def __next__(self):
+    def __next__(self) -> Any:
+        """Return the next value in the array
+        :raises StopIterator: when end of array is reached"""
         if self.cur_index >= len(self.dynamic_array):
             raise StopIteration
         return_data = self.dynamic_array.get(self.cur_index)
